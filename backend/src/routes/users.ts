cf2eb6cde2186729582559816239ddf9d8d4a7ab
Rama Raju B <142677132+ramraj1096@users.noreply.github.com>
2024-03-12 23:24:08 +0530

    Update users.ts

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,9 +2,6 @@ import express, { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
-
-import verifyToken from "../middleware/auth";
-
 import verifyToken from '../middleware/auth';
 import cors from "cors";
 
